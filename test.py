c41b6a7010ce9eb14143f2ea0a88b81ccb3073f2
nickhib <root@LAPTOP-MMSI4IG9.localdomain>
2023-11-13 14:51:15 -0800

    added dockerfile and tests

---
+++ test.py
@@ -0,0 +1,8 @@
+# Simple Python Program
+
+# Get user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+# Print a personalized greeting
+print(f"Hello, {name}! You are {age} years old.")

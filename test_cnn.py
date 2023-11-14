10837498db6a7ea3f517767025a422e6df8990b0
khasek <kmhasek@gmail.com>
2023-11-13 21:13:20 -0800

    Added error handling for cnn test args

--- test_cnn.py
+++ test_cnn.py
@@ -4,7 +4,7 @@ import sys
 import tensorflow as tf
 
 # Make sure a model has been provided
-if (len(sys.argv) > 2):
+if (len(sys.argv) < 2):
   print("Please provide at least one CNN model: python test_cnn.py myCNN.model")
   quit()
 
@@ -17,11 +17,15 @@ test_pixels = tf.keras.utils.normalize(test_pixels, axis=1)
 i = 1
 while (i < len(sys.argv)):
 
-  cnn = tf.keras.models.load_model(sys.argv[i])
-  loss, accuracy = cnn.evaluate(test_pixels, test_labels)
+  print("\nModel", i)
 
-  print("Model", i)
-  print("Accuracy:", accuracy)
-  print("Loss:", loss, "\n")
+  try:
+    cnn = tf.keras.models.load_model(sys.argv[i])
+  except:
+    print("Unable to test", sys.argv[i], "\n")
+  else:
+    loss, accuracy = cnn.evaluate(test_pixels, test_labels)
+    print("Accuracy:", accuracy)
+    print("Loss:", loss, "\n")
 
   i += 1

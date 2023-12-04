b8537347c59825a52898a09336c45dd80d6c7335
no-bugs-only-features <dwright2@mail.csuchico.edu>
2023-12-04 08:40:31 -0800

    Refactor AlexNet

--- alexnet.py
+++ alexnet.py
@@ -24,45 +24,43 @@ y_train = y_train[:-2000]
 
 from tensorflow.keras import models, layers, losses
 
-# model = models.Sequential()
+model = models.Sequential()
 
-# model.add(layers.experimental.preprocessing.Resizing(224, 224, interpolation="bilinear", input_shape=x_train.shape[1:]))
+model.add(layers.experimental.preprocessing.Resizing(224, 224, interpolation="bilinear", input_shape=x_train.shape[1:]))
 
-# model.add(layers.Conv2D(96, 11, strides=4, padding='same'))
-# model.add(layers.Lambda(tf.nn.local_response_normalization))
-# model.add(layers.Activation('relu'))
-# model.add(layers.MaxPooling2D(3, strides=2))
+model.add(layers.Conv2D(96, 11, strides=4, padding='same'))
+model.add(layers.Lambda(tf.nn.local_response_normalization))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(3, strides=2))
 
-# model.add(layers.Conv2D(256, 5, strides=4, padding='same'))
-# model.add(layers.Lambda(tf.nn.local_response_normalization))
-# model.add(layers.Activation('relu'))
-# model.add(layers.MaxPooling2D(3, strides=2))
+model.add(layers.Conv2D(256, 5, strides=4, padding='same'))
+model.add(layers.Lambda(tf.nn.local_response_normalization))
+model.add(layers.Activation('relu'))
+model.add(layers.MaxPooling2D(3, strides=2))
 
-# model.add(layers.Conv2D(384, 3, strides=4, padding='same'))
-# model.add(layers.Activation('relu'))
+model.add(layers.Conv2D(384, 3, strides=4, padding='same'))
+model.add(layers.Activation('relu'))
 
-# model.add(layers.Conv2D(256, 3, strides=4, padding='same'))
-# model.add(layers.Activation('relu'))
+model.add(layers.Conv2D(256, 3, strides=4, padding='same'))
+model.add(layers.Activation('relu'))
 
-# model.add(layers.Flatten())
+model.add(layers.Flatten())
 
-# model.add(layers.Dense(4096, activation='relu'))
-# model.add(layers.Dropout(0.5))
+model.add(layers.Dense(4096, activation='relu'))
+model.add(layers.Dropout(0.5))
 
-# model.add(layers.Dense(4096, activation='relu'))
-# model.add(layers.Dropout(0.5))
+model.add(layers.Dense(4096, activation='relu'))
+model.add(layers.Dropout(0.5))
 
-# model.add(layers.Dense(10, activation='softmax'))
+model.add(layers.Dense(10, activation='softmax'))
 
-# model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
-
-# model.save("AlexNet.model")
-
-model = models.load_model("AlexNet.model")
+model.compile(optimizer='adam', loss=losses.sparse_categorical_crossentropy, metrics=['accuracy'])
 
 model.summary()
 
 history = model.fit(x_train, y_train, batch_size=64, epochs=40, validation_data=(x_val, y_val))
+
+model.save("AlexNet.model")
 
 fig, axis = plt.subplots(2, 1, figsize=(15,15))
 axis[0].plot(history.history['loss'])
@@ -77,4 +75,6 @@ axis[1].legend(['Train', 'Val'])
 
 plt.show()
 
-#model.evaluate(x_test, y_test)
+# model = models.load_model("AlexNet.model")
+
+# model.evaluate(x_test, y_test)
